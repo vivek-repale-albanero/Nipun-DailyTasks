7e90104c8a8ddd0f976715426c12add002ed9989
Nipun79 <bathininipun79@gmail.com>
2021-09-23 19:58:04 +0530

    day-6

--- Day-5/web-page/index.js
+++ Day-5/web-page/index.js
@@ -1,24 +1,49 @@
-btn.addEventListener("click",()=>{
-    return console.log("hi")
-})
+
 fetch("./api.json").then((response)=>{
     return response.json();
 }).then((data)=>{
-    let product =document.getElementById("hello")
+    let html = '';
     for(let i=0;i<data.length;i++){
-        product.innerHTML=`
-    <div class="product-container>
-    ${data.map((item,index) => {
-    return` 
-    <img class="product_image" src=${(item+1).product_image}>
-    <div class="info">
-    <h2 class='name'> ${data[index+1].name}</h2>
-    <p class="desc"> ${item.desc} </p>
-    <div class="price">Rs.${item.price}</div>
-    <a class="btn">Add to cart </a>`}).join('')}
-    </div>`;}
+       html=`
+      
+       ${data.map((item,index) => {
+           return` 
+           <div class="container">
+           <div class="row">
+           <div class="col">
+           <div class="product-container">
+           <img class="product_image" src="${item.product_image}" />
+           <div class="info">
+             <h2 class="name">${item.name}</h2>
+             <p class="desc">${item.desc}</p>
+             <div class="price">${item.price}</div>
+             <button type="button" id="btn">Add to cart</button>
+           </div>
+           </div>
+           </div>
+           </div>
+           </div>`}).join('')}
+    <br> <br><br>` ;}
+    hello.innerHTML= html
 }).catch((error)=>{
     return console.log(error)
 })
 
 
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
