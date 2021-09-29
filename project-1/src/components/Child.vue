1ea8099bf3c143c078c592ef21dcfac242c0b2bf
Nipun79 <bathininipun79@gmail.com>
2021-09-29 13:11:16 +0530

    users latest

---
+++ project-1/src/components/Child.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        
+   
+    <div class="container">
+  <div class="form-group row">
+    <label for="colFormLabel" class="col-sm-2 col-form-label ">Name:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control form-control-sm mx-1" id="colFormLabel" v-model="Name" >
+    </div>
+  </div> 
+  <div class="form-group row">
+    <label for="colFormLabel" class="col-sm-2 col-form-label">City:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control form-control-sm mx-1" id="colFormLabel" v-model="City" >
+    </div>
+  </div> 
+  <button @click="push">push</button>
+
+</div>
+
+  </div>
+</template>
+
+<script>
+import subChild from "./subChild.vue"
+export default {
+components:{
+subChild
+},
+props:["array"],
+data(){
+    return{
+        Name:"",
+        City:"",
+        show:false
+    }
+},
+methods:{
+  push(){
+  this.array.push({Name:this.Name,City:this.City})
+  console.log(array)
+  }
+},
+mounted(){
+    
+}
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+}
+.card-text{
+    float: left;
+}
+</style>
