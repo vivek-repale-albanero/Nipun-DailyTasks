d264f64670568930176b872828935b8326cec113
Nipun79 <bathininipun79@gmail.com>
2021-09-30 20:21:50 +0530

    commit

---
+++ Typescript/basics.ts
@@ -0,0 +1,17 @@
+//Core types
+// function add(a:number,b:number,printResult:Boolean){
+//     if(printResult)
+//     return a+b
+//     else
+//     return "false"
+    
+// }
+// let printResult=false
+// console.log(add(8,0,printResult))
+// //typinference : typscript recognize the type and we can't chenge the type
+// let a =5
+// // a='a'
+
+// // let number1:number (number is type)
+
+
